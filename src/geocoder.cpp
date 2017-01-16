616c96db17695ea98f97c93906be32b9c525ccf3
Rinigus <rinigus.git@gmail.com>
2017-01-16 11:36:39 +0200

    use joins on queries, fixes #7

--- src/geocoder.cpp
+++ src/geocoder.cpp
@@ -13,67 +13,67 @@ Geocoder::Geocoder()
 
 Geocoder::Geocoder(const std::string &dbname)
 {
-    if ( !load(dbname) )
-        std::cerr << "Geocoder: error loading " << dbname << std::endl;
+  if ( !load(dbname) )
+    std::cerr << "Geocoder: error loading " << dbname << std::endl;
 }
 
 bool Geocoder::load(const std::string &dbname)
 {
-    return (m_db.connect(dbname.c_str(), SQLITE_OPEN_READONLY) == SQLITE_OK);
+  return (m_db.connect(dbname.c_str(), SQLITE_OPEN_READONLY) == SQLITE_OK);
 }
 
 void Geocoder::drop()
 {
-    m_db.disconnect();
+  m_db.disconnect();
 }
 
 void Geocoder::search(const std::vector<Postal::ParseResult> &parsed_query, std::vector<Geocoder::GeoResult> &result)
 {
-    // parse query by libpostal
-    std::vector< std::vector<std::string> > parsed_result;
-    Postal::result2hierarchy(parsed_query, parsed_result);
+  // parse query by libpostal
+  std::vector< std::vector<std::string> > parsed_result;
+  Postal::result2hierarchy(parsed_query, parsed_result);
 
-    result.clear();
-    m_levels_resolved = 0;
+  result.clear();
+  m_levels_resolved = 0;
 
 #ifdef GEONLP_PRINT_DEBUG
-    std::cout << "Search hierarchies:\n";
+  std::cout << "Search hierarchies:\n";
 #endif
 #ifdef GEONLP_PRINT_DEBUG_QUERIES
-    std::cout << "\n";
+  std::cout << "\n";
 #endif
 
-    for (const auto &r: parsed_result)
+  for (const auto &r: parsed_result)
     {
 #ifdef GEONLP_PRINT_DEBUG
-        for (auto a: r)
-            std::cout << a << " / ";
-        std::cout << "\n";
+      for (auto a: r)
+	std::cout << a << " / ";
+      std::cout << "\n";
 #endif
 
-        m_query_count = 0;
-        search(r, result);
+      m_query_count = 0;
+      search(r, result);
 #ifdef GEONLP_PRINT_DEBUG_QUERIES
-        std::cout << "\n";
+      std::cout << "\n";
 #endif
     }
 
 #ifdef GEONLP_PRINT_DEBUG
-        std::cout << "\n";
+  std::cout << "\n";
 #endif
 
-    // fill the data
-    for (GeoResult &r: result)
+  // fill the data
+  for (GeoResult &r: result)
     {
-        get_name(r.id, r.title, r.address, m_levels_in_title);
+      get_name(r.id, r.title, r.address, m_levels_in_title);
 
-        sqlite3pp::query qry(m_db, "SELECT latitude, longitude FROM object_primary WHERE id=?");
-        qry.bind(1, r.id);
-        for (auto v: qry)
+      sqlite3pp::query qry(m_db, "SELECT latitude, longitude FROM object_primary WHERE id=?");
+      qry.bind(1, r.id);
+      for (auto v: qry)
         {
-            // only one entry is expected
-            v.getter() >> r.latitude >> r.longitude;
-            break;
+	  // only one entry is expected
+	  v.getter() >> r.latitude >> r.longitude;
+	  break;
         }
     }
 }
@@ -81,106 +81,125 @@ void Geocoder::search(const std::vector<Postal::ParseResult> &parsed_query, std:
 bool Geocoder::search(const std::vector<std::string> &parsed, std::vector<Geocoder::GeoResult> &result, size_t level,
                       long long int range0, long long int range1)
 {
-    if ( level >= parsed.size() || (m_max_queries_per_hierarchy>0 && m_query_count > m_max_queries_per_hierarchy) )
-        return false;
+  if ( level >= parsed.size() || (m_max_queries_per_hierarchy>0 && m_query_count > m_max_queries_per_hierarchy) )
+    return false;
 
-    m_query_count++;
+  m_query_count++;
 
-    std::string extra;
-    if (level > 0)
-        extra = "AND prim_id>? AND prim_id<=?";
+  std::string extra;
+  if (level > 0)
+    extra = "AND nn.prim_id>? AND nn.prim_id<=?";
 
-    std::deque<long long int> ids;
+  std::deque<long long int> ids;
+  std::deque<long long int> last_subobj;
 
-    std::string command =
-            "SELECT prim_id FROM normalized_name WHERE name GLOB \"" + parsed[level] +
-            "*\" " + extra + " ORDER BY length(name)";
+  std::string command;
+  bool last_level = ( level+1 >= parsed.size() );
+
+  if ( last_level ) // this is the last level we are interested in
+    command =
+      "SELECT DISTINCT nn.prim_id FROM normalized_name nn WHERE nn.name GLOB \"" + parsed[level] +
+      "*\" " + extra + " ORDER BY length(name)";
+
+  else // we would be interested in sub-objects
+    {
+      command = "SELECT DISTINCT nn.prim_id, h.last_subobject FROM normalized_name nn ";
+
+      if (m_levels_resolved <= level+1) // any result could improve
+	// the current best hit
+	command += "LEFT ";
+
+      command +=
+	"JOIN hierarchy h ON nn.prim_id=h.prim_id "
+	"WHERE nn.name GLOB \"" + parsed[level] +
+	"*\" " + extra + " ORDER BY length(nn.name)";
+    }
 
 #ifdef GEONLP_PRINT_DEBUG_QUERIES
-    std::cout << level << " " << command << "\n";
+  std::cout << level << " " << command << "\n";
 #endif
-    sqlite3pp::query qry(m_db, command.c_str());
-    if (level > 0)
+  sqlite3pp::query qry(m_db, command.c_str());
+  if (level > 0)
     {
-        qry.bind(1, range0);
-        qry.bind(2, range1);
+      qry.bind(1, range0);
+      qry.bind(2, range1);
     }
 
-    for (auto v : qry)
+  for (auto v : qry)
     {
-        long long int id;
-        v.getter() >> id;
-        ids.push_back(id);
+      long long int id;
+      v.getter() >> id;
+      ids.push_back(id);
+
+      if ( !last_level )
+	{
+	  long long int r1 = -1;
+	  bool null_type = (v.column_type(1) == SQLITE_NULL);
+	  if (!null_type) r1 = v.get<long long int>(1);
+	  last_subobj.push_back(r1);
+	}
     }
 
-    for (long long int id: ids)
+  for (size_t id_i = 0; id_i < ids.size(); ++id_i)
     {
-        long long int r1 = -1;
+      long long int id = ids[id_i];
+      if ( last_level ||
+	   last_subobj[id_i] <= id ||
+	   !search(parsed, result, level+1, id, last_subobj[id_i]) )
         {
-            sqlite3pp::query qry(m_db, "SELECT last_subobject FROM hierarchy WHERE prim_id=?");
-            qry.bind(1, id);
-            for (auto v : qry )
+	  size_t levels_resolved = level+1;
+	  if ( m_levels_resolved < levels_resolved )
             {
-                v.getter() >> r1;
-                break; // only one value is expected
-            }
-        }
-
-        if ( r1 <= id || !search(parsed, result, level+1, id, r1) )
-        {
-            size_t levels_resolved = level+1;
-            if ( m_levels_resolved < levels_resolved )
-            {
-                result.clear();
-                m_levels_resolved = levels_resolved;
+	      result.clear();
+	      m_levels_resolved = levels_resolved;
             }
 
-            if (m_levels_resolved == levels_resolved && (m_max_results==0 || result.size() < m_max_results))
+	  if (m_levels_resolved == levels_resolved && (m_max_results==0 || result.size() < m_max_results))
             {
-                bool have_already = false;
-                for (const auto &r: result)
-                    if (r.id == id)
-                    {
-                        have_already = true;
-                        break;
-                    }
+	      bool have_already = false;
+	      for (const auto &r: result)
+		if (r.id == id)
+		  {
+		    have_already = true;
+		    break;
+		  }
 
-                if (!have_already)
+	      if (!have_already)
                 {
-                    GeoResult r;
-                    r.id = id;
-                    r.levels_resolved = levels_resolved;
-                    result.push_back(r);
+		  GeoResult r;
+		  r.id = id;
+		  r.levels_resolved = levels_resolved;
+		  result.push_back(r);
                 }
             }
         }
     }
 
-    return ids.size() > 0;
+  return ids.size() > 0;
 }
 
 void Geocoder::get_name(long long id, std::string &title, std::string &full, int levels_in_title)
 {
-    long long int parent;
-    std::string name;
+  long long int parent;
+  std::string name;
 
-    sqlite3pp::query qry(m_db, "SELECT name, parent FROM object_primary WHERE id=?");
-    qry.bind(1, id);
-    for (auto v: qry)
+  sqlite3pp::query qry(m_db, "SELECT name, parent FROM object_primary WHERE id=?");
+  qry.bind(1, id);
+  for (auto v: qry)
     {
-        // only one entry is expected
-        v.getter() >> name >> parent;
+      // only one entry is expected
+      v.getter() >> name >> parent;
 
-        if (!full.empty()) full += ", ";
-        full += name;
+      if (!full.empty()) full += ", ";
+      full += name;
 
-        if (levels_in_title > 0)
+      if (levels_in_title > 0)
         {
-            if (!title.empty()) title += ", ";
-            title += name;
+	  if (!title.empty()) title += ", ";
+	  title += name;
         }
 
-        get_name(parent, title, full, levels_in_title-1);
-        return;
+      get_name(parent, title, full, levels_in_title-1);
+      return;
     }
 }
