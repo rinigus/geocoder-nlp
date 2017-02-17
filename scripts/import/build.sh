771c18acb3d8ec4b6f38475c4d4673e2f6be656d
rinigus <rinigus.git@gmail.com>
2017-02-17 04:45:50 +0000

    scripts for uploading imported maps

--- scripts/import/build.sh
+++ scripts/import/build.sh
@@ -2,37 +2,35 @@
 
 set -e
 
-OSMSCOUT_IMPORTER=/usr/local/osmscout/bin/Import
+OSMSCOUT_IMPORTER=../../../../osmscout-install/bin/Import
 OSMSCOUT_MAPSTYLE=../../importer/stylesheet/map.ost
 GEOCODER_IMPORTER=../../importer/importer
 
+export LD_LIBRARY_PATH=../../../../osmscout-install/lib:../../../install/lib
+
 if [ $# -lt 5 ] ; then
-	echo "Usage: $0 mapfile_pbf world_dir continent country country_2_letter_code"
+	echo "Usage: $0 mapfile_pbf base_dir continent country country_2_letter_code"
 	exit 1
 fi
 
 PBF=$1
-WORLD_DIR=$2
+BASE_DIR=$2
 CONTINENT=$3
 COUNTRY=$4
 COUNTRY_CODE=$5
 
-FULLDIR=full/$WORLD_DIR
-IMPDIR="$FULLDIR/$CONTINENT/$COUNTRY"
-SQL="$WORLD_DIR/$CONTINENT/$COUNTRY.sqlite"
+IMPDIR="$BASE_DIR/osmscout/$CONTINENT/$COUNTRY"
+SQLDIR="$BASE_DIR/geocoder-nlp/$CONTINENT/$COUNTRY"
+SQL="$SQLDIR/location.sqlite"
 
+rm -rf "$IMPDIR" "$SQLDIR"
 mkdir -p "$IMPDIR"
+mkdir -p "$SQLDIR"
 
 "$OSMSCOUT_IMPORTER" --typefile "$OSMSCOUT_MAPSTYLE" --delete-temporary-files true --delete-debugging-files true --delete-analysis-files true --delete-report-files true --destinationDirectory "$IMPDIR" "$PBF"
 
 "$GEOCODER_IMPORTER" "$IMPDIR" "$SQL" "$COUNTRY_CODE"
 
-# cleanup older version
-rm -rf "$IMPDIR.tar.bz2"
-rm -rf "$SQL.bz2"
+./pack.sh "$IMPDIR" 10
+./pack.sh "$SQLDIR" 1
 
-# compress and delete libosmscout dir
-(cd "$IMPDIR/.." && tar jcvf "$COUNTRY.tar.bz2" "$COUNTRY")
-bzip2 "$SQL"
-
-rm -rf "$IMPDIR"
---
+++ scripts/import/uploader.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+./upload_command.sh distribution/geocoder-nlp/europe/estonia geocoder-nlp-1/geocoder-nlp/europe/estonia
+./upload_command.sh distribution/osmscout/europe/estonia osmscout-1/osmscout/europe/estonia
+./upload_command.sh distribution/postal/countries/EE postal-country-1/postal/countries/EE
+./upload_command.sh distribution/postal/global postal-global-1/postal/global
+./upload_command.sh distribution/geocoder-nlp/europe/andorra geocoder-nlp-1/geocoder-nlp/europe/andorra
+./upload_command.sh distribution/osmscout/europe/andorra osmscout-1/osmscout/europe/andorra
+./upload_command.sh distribution/postal/countries/AD postal-country-1/postal/countries/AD
+./upload_command.sh distribution/geocoder-nlp/europe/albania geocoder-nlp-1/geocoder-nlp/europe/albania
+./upload_command.sh distribution/osmscout/europe/albania osmscout-1/osmscout/europe/albania
+./upload_command.sh distribution/postal/countries/AL postal-country-1/postal/countries/AL
